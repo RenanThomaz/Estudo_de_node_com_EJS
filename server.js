e797e1630eb25c65a60c692624f87ca6f6ff477d
RenanThomaz <renanthomaz2@gmail.com>
2022-04-25 11:24:03 -0300

    aula final de node com ejs, utilizando o foreach para criar elementos html

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const app = express();
 
 app.set("view engine","ejs");
 
 app.get("/", function(req,res){
-    res.render("pages/index")
+    const items = [
+        {title: "D" , message: "esenvolver aplicações/serviços de forma fácil"},
+        {title: "E" , message: "JS usa javascript para renderizar HTML"},
+        {title: "M" , message: "uito fácil de usar"},
+        {title: "A" , message: "morzinho"},
+        {title: "I" , message: "nstall EJS"},
+        {title: "S" , message: "intaxe simples"},
+    ];
+
+    const subtitle = " Uma linguagem de modelagem para criação de página HTLM"
+
+    res.render("pages/index",{
+        qualitys: items,
+        subtitle: subtitle
+    })
 })
+
+
+
 
 
 app.get("/sobre", function(req,res){
